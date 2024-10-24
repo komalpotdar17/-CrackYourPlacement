c7929586402d3b6f8f5aa08bdd685e952fda3af1
Komal Potdar <komalpotdar52@gmail.com>
2024-10-25 00:31:32 +0530

    Time: 262 ms (61.07%), Space: 42.2 MB (68.50%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,21 +1,20 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
         int minRepeats = (b.length() + a.length() - 1) / a.length();
-        StringBuilder repeatedA = new StringBuilder();
         
-        for (int i = 0; i < minRepeats; i++) {
-            repeatedA.append(a);
-        }
+        String repeated = a.repeat(minRepeats);
         
-        if (repeatedA.indexOf(b) != -1) {
+        if (repeated.contains(b)) {
             return minRepeats;
         }
         
-        repeatedA.append(a);
-        if (repeatedA.indexOf(b) != -1) {
+        repeated += a;
+        
+        if (repeated.contains(b)) {
             return minRepeats + 1;
         }
         
         return -1;
+
     }
 }
