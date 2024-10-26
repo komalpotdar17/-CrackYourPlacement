b880aff04834464869e19d85f5e23b2e2a926de6
Komal Potdar <komalpotdar52@gmail.com>
2024-10-27 00:31:04 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (21.81%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,22 +1,27 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] v1 = version1.split("\\.");
-        String[] v2 = version2.split("\\.");
-        
-        int maxLength = Math.max(v1.length, v2.length);
-        
-        for (int i = 0; i < maxLength; i++) {
-            
-            int num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
-            int num2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
-            
-        if (num1 < num2) {
-                return -1;
-            } else if (num1 > num2) {
-                return 1;
+        int i = 0, j = 0;
+
+        while (i < version1.length() || j < version2.length()) {
+            int num1 = 0, num2 = 0;
+
+            while (i < version1.length() && version1.charAt(i) != '.') {
+                num1 = num1 * 10 + (version1.charAt(i) - '0');
+                i++;
             }
+
+            while (j < version2.length() && version2.charAt(j) != '.') {
+                num2 = num2 * 10 + (version2.charAt(j) - '0');
+                j++;
+            }
+
+            if (num1 < num2) return -1;
+            if (num1 > num2) return 1;
+
+            i++;
+            j++;
         }
-        
+
         return 0;
     }
 }
