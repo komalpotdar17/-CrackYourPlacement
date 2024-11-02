9e7a745d0c5230b46c0f29e704f8de2a4f765dcc
Komal Potdar <komalpotdar52@gmail.com>
2024-11-02 21:38:21 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (19.35%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -3,12 +3,13 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
-            }
+        int low = 0, high = n*m-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            int row = mid/m, col = mid%m;
+            if(matrix[row][col]==target) return true;
+            else if(matrix[row][col]<target) low=mid+1;
+            else high=mid-1;
         }
         
         return false;
